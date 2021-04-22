d30f1d621709f00322a48a0928b3ab2148f624f3
NamasteIndia <49607060+NamasteIndia@users.noreply.github.com>
2021-04-22 22:26:15 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -15,8 +15,8 @@ async def media_receive_handler(c: Client, m: Message):
                                 Var.PORT,
                                 log_msg.message_id)
     await m.reply_text(
-        text="`{}`".format(stream_link),
-        text="A program by @mirrorupdatesnnews",
-        text="Join for unlimited TD, and much more stuffs",
+        text="`{}`visit @mirrorupdatesnnews".format(stream_link),
+  
+
         quote=True
     )
